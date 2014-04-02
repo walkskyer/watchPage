3db5269b3ad37819afffac68f96d5371e7c24330
walkskyer <walkskyer@qq.com>
2014-04-02 22:48:31 +0800

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# filename:setup.py
+# datetime:2014-03-30 19:34
+__author__ = 'walkskyer'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="watchPage",
+    version="1.0",
+    url='',
+    author='walkskyer',
+    author_email='模拟浏览器',
+    description='',
+    license='MIT',
+    packages=find_packages(),
+    package_data={},
+    scripts=[],
+)
---
+++ watchPage/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# filename:__init__.py
+# datetime:2014-04-02 21:04
+__author__ = 'walkskyer'
+"""
+
+"""
+
+if __name__ == "__main__":
+    pass
